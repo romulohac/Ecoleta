b756e7c399689103282dddcbbfc2a7145080b26d
Rômulo Henrique <romulo.hac@gmail.com>
2020-06-25 14:10:43 -0300

    alteraçoes e novas funcionalidades e arquivos feitos 25/06/2020

---
+++ scripts/intro.js
@@ -0,0 +1,61 @@
+//document.write("Hello")
+
+//let myvar = "kkkkkk o cara é arrombado"
+
+//const myconst ='he'
+//document.write(myconst + myvar)
+
+//const s1 = 'isso é uma string'
+
+
+//document.write(s1)
+
+//objeto possuem propriedade e funcionalidades
+
+//const pessoa = {
+  //altura : "1,80m",
+  //idade: 24,
+ // solteiro: true,
+ // correr(){
+    
+   //document.write("executar uma grande logica de correr")
+ // }
+  
+  
+//}//
+
+//pessoa.correr()//
+
+
+
+//const colecaoDeBolinhas = ["blue", "green", 1, {name:"My name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+
+//function sayMyName(name){
+  
+  //document.write(name)
+//}
+
+//sayMyName("douglas")
+//sayMyName("valeska")
+//sayMyName("kyam")
+
+//const notafinal = 4
+  
+ // if( notafinal < 5 ){
+   // document.write("Reprovado")
+    
+ // } else {
+    // document.write("Aprovado")
+   // }
+    
+  
+    
+ //loop repetiçoes
+
+// for ( i = 0 ; i <10; i++){
+  //  document.write(`<p>${i}</p>`)
+  //}
+    
